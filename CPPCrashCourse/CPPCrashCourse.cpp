93744150666d9514b0cff7777f23bd2189cb2ef4
Giot08 <xgiot08@gmail.com>
2024-12-04 22:30:43 -0300

    update

--- CPPCrashCourse/CPPCrashCourse.cpp
+++ CPPCrashCourse/CPPCrashCourse.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 enum EyeColor { Blue, Brown, Green, Gray };
 
+// Functions
+
+float sum(float a, float b);
+void greet(string name, string country = "chile");
+
+void celebrateBirthday(int* age);
+
 int main()
 {
     // Hello World
     // cout << "Welcome to console\n";
     
     // Variables
+    /*
     int age = 30;
     float weigth = 80.25;
     double balance = 123123.294;
@@ -18,6 +26,7 @@ int main()
         // string colors[3] = { "red", "white", "orange" };
         // string colors[3] = {  };
     string colors[] = { "red", "white", "orange" };
+    */
     // cout << colors[0] + "\n";    
     // cin >> colors[0];
     // cout << colors[0] + "\n";
@@ -34,9 +43,9 @@ int main()
     //// Ternario
     // isWeekend ? cout << "Be happy" : cout << "Dont be happy, go to work!";
 
+    /*
     int week = 4;
     isWeekend = true;
-    /*
         if (isWeekend && week == 4) {
             cout << "Fin de semana y con platita" << endl;
         }
@@ -71,19 +80,56 @@ int main()
     */
 
     // Bucles
+    /*
+    cout << "While: \n";
+    int whileCounter = 20;
 
+    while (whileCounter > 1) {
+        cout << whileCounter << endl;
+        whileCounter--;
+    }
 
+    cout << "DO While: \n";
+    whileCounter = 20;
+    do {
+        cout << whileCounter << endl;
+        whileCounter--;
+    } while (whileCounter > 20);
 
+    string animals[6] = { "cat", "dog", "bird", "fish", "bear", "Gecko" };
 
+    for (int i = 0; i < 6; i++) {
+        cout << animals[i] << endl;
+    }
+    */
 
+    // Functions
+    /*
+    float result = sum(2.4, 4.6);
+    cout << result << endl;
 
+    greet("Jose", "Venezuela");
 
+    */
 
-
-
-
-
-
+    // Pointers
+    int myAge = 20;
+    celebrateBirthday(&myAge);
+    cout << myAge << endl;
 
     system("pause>0");
+}
+
+
+float sum(float a, float b) {
+    return a + b; 
+}
+
+void greet(string name, string country) {
+    cout << name << ", soy de " << country << endl;
+};
+
+void celebrateBirthday(int* age) {
+    (*age)++;
+    cout << "Bday: " << *age << endl;
 }
